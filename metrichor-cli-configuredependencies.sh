b8f26fb61b33c9522e36ec1459edd5720514c385
Alexis Lucattini <awlucattini@icloud.com>
2016-10-02 14:46:27 +1100

    Fixed some hard-coded variables in the bash sources.

--- metrichor-cli-configuredependencies.sh
+++ metrichor-cli-configuredependencies.sh
@@ -16,9 +16,9 @@ metrichor_version="2.40.16"
 gcc_version="5.4.0"
 
 # Set and export compiler flags
-export CXX=$HOME/.local/easybuild/software/GCC/$gcc_version/bin/g++
-export CC=$HOME/.local/easybuild/software/GCC/$gcc_version/bin/gcc
-export LD_LIBRARY_PATH=$HOME/.local/easybuild/software/GCC/$gcc_version/lib/:$HOME/.local/easybuild/software/GCC/$gcc_version/lib64/:$LD_LIBRARY_PATH
+export CXX=$HOME/.local/easybuild/software/GCC/${gcc_version}/bin/g++
+export CC=$HOME/.local/easybuild/software/GCC/${gcc_version}/bin/gcc
+export LD_LIBRARY_PATH=$HOME/.local/easybuild/software/GCC/${gcc_version}/lib/:$HOME/.local/easybuild/software/GCC/${gcc_version}/lib64/:${LD_LIBRARY_PATH}
 
 # Set hdf5 flag
 HDF5_HOME=$HOME/src/hdf5/
@@ -29,25 +29,25 @@ cd $HOME/nvm-master
 
 
 export NVM_DIR=$HOME/.nvm
-source $NVM_DIR/nvm.sh  # This loads nvm
+source ${NVM_DIR}/nvm.sh  # This loads nvm
 
 #Change to metrichor directory and install nvm and npm
-cd $HOME/metrichor-cli-$metrichor_version
+cd $HOME/metrichor-cli-${metrichor_version}
 
-nvm install $nvm_version
+nvm install ${nvm_version}
 
 # npm needs to be installed globally, otherwise I get errors I haven't been able to fix.
 # export path to npm and node
-# export PATH=$PATH:$HOME/.nvm/versions/node/v5.12.0/bin/
+export PATH=$PATH:$HOME/.nvm/versions/node/v${nvm_version}/bin/
 npm install npm -g
 
 npm install hdf5 -ws:verbose -g
 
 # export path to metrichor commandline agent
-export PATH=$PATH:$HOME/metrichor-cli-2.40.16/bin/
+export PATH=$PATH:$HOME/metrichor-cli-${metrichor_version}/bin/
 
 # export node path to link to hdf5 module
-export NODE_PATH=$HOME/.nvm/versions/node/v5.12.0/lib/node_modules/
+export NODE_PATH=$HOME/.nvm/versions/node/v${nvm_version}/lib/node_modules/
 
 # change back to current directory
-cd $current_directory
+cd ${current_directory}
--- metrichor-cli-setpaths.sh
+++ metrichor-cli-setpaths.sh
@@ -9,23 +9,25 @@ gcc_version="5.4.0"
 # Set and export compiler flags
 export CXX=$HOME/.local/easybuild/software/GCC/$gcc_version/bin/g++
 export CC=$HOME/.local/easybuild/software/GCC/$gcc_version/bin/gcc
-export LD_LIBRARY_PATH=$HOME/.local/easybuild/software/GCC/$gcc_version/lib/:$HOME/.local/easybuild/software/GCC/$gcc_version/lib64/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$HOME/.local/easybuild/software/GCC/${gcc_version}/lib/:$HOME/.local/easybuild/software/GCC/${gcc_version}/lib64/:${LD_LIBRARY_PATH}
 
 # Set hdf5 flag
 HDF5_HOME=$HOME/src/hdf5/
 
 # Load nvm
 export NVM_DIR=$HOME/.nvm
-source $NVM_DIR/nvm.sh  # This loads nvm
+source ${NVM_DIR}/nvm.sh  # This loads nvm
 
 # source API_KEY
 source $HOME/.met_apikey
 
+# To ensure that your api key is sourced type "echo $MET_APIKEY" into the commandline
+
 # export path to metrichor commandline agent
-export PATH=$PATH:$HOME/metrichor-cli-2.40.16/bin/
+export PATH=$PATH:$HOME/metrichor-cli-${metrichor_version}/bin/
 
 # export node path to link to hdf5 module
-export NODE_PATH=$HOME/.nvm/versions/node/v${nvm_version}/lib/node_modules/:$NODE_PATH
+export NODE_PATH=$HOME/.nvm/versions/node/v${nvm_version}/lib/node_modules/:${NODE_PATH}
 
 
 
